95792e38943068ce1465b1b977e5d8482f4bb53b
Arisha-Akbar <arishaakbar713@gmail.com>
2025-12-07 15:56:47 +0500

    Initialized React Router project with Tailwind CSS and Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-500">react router</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./components/Home/Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import About from "./components/About/About.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      }
+      ,{
+        path: "",
+        element: <About/> 
+      
+      }
+    ]
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
